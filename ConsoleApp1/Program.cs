33da9b0b912f5c285e96ae1359089d32ce410381
247806 <247806@edu.p.lodz.pl>
2024-03-06 14:28:44 +0100

    Testy jednostkowe

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,17 @@
-﻿internal class Program
+﻿
+public class Program
 {
+    public static int func(int a, int b)
+    {
+        return a + b;
+    }
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-
-
+        int a = 2; 
+        int b = 3;
         Console.WriteLine("Hej!");
+        int result = func(a, b);
     }
+
 }
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            int a = 2;
+            int b = 3;
+            int result = Program.func(a, b);
+
+            Assert.AreEqual(5, result);
+        }
+    }
+}
