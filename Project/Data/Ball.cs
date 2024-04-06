abb81cfe7a3b7d14e86a86edcdd65a4bcce06182
247806 <247806@edu.p.lodz.pl>
2024-04-07 01:49:31 +0200

    View/ViewModel beginnings

--- Project/Data/Ball.cs
+++ Project/Data/Ball.cs
@@ -2,8 +2,35 @@
 
 namespace Data
 {
-    public class Class1
+    public class Ball
     {
+        private double posX;
+        private double posY;
+        private double a;
 
+        public Ball(double x, double y, int direction)
+        {
+            this.posX = x;
+            this.posY = y;
+            this.a = y / x * direction; 
+        }
+
+        public double PosX
+        {
+            get { return posX; }
+            set { posX = value; }
+        }
+
+        public double PosY
+        {
+            get { return posY; }
+            set { posY = value; }
+        }
+
+        public double A
+        {
+            get { return a; }
+            set { a = value; }
+        }
     }
 }
--- Project/Presentation/ViewModel/Class1.cs
+++ Project/Presentation/ViewModel/Class1.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace ViewModel
 {
-    public class Class1
+    public class Class1 : ICommand
     {
+        public event EventHandler CanExecuteChanged;
 
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
