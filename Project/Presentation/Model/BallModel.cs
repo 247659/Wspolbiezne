9ddc9db105aec4a6319ce643964bd2398d206373
Jan Śladowski <247806@edu.p.lodz.pl>
2024-04-07 23:54:57 +0200

    Working on presentation

---
+++ Project/Presentation/Model/BallModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Model
+{
+    public class BallModel : INotifyPropertyChanged
+    {
+        private double _posX;
+        private double _posY;
+        private double velocity;
+
+        public double PosX
+        {
+            get { return PosX; }
+            set
+            {
+                _posX = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double PosY
+        {
+            get { return PosY; }
+            set
+            {
+                _posY = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Project/Presentation/ViewModel/VievModelController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class VievModelController : ICommand, INotifyPropertyChanged
+    {
+        private string _ballsNumber;
+
+        public VievModelController()
+        {
+            BallsNumber = "0";
+        }
+
+        public string BallsNumber
+        {
+            get { return _ballsNumber; }
+            set
+            {
+                _ballsNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
