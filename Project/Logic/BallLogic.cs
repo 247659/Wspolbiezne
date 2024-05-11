9cb7ca8295f49ed53c57c670d055bb42e44ea8ed
247806 <247806@edu.p.lodz.pl>
2024-05-11 23:00:48 +0200

    Threads for balls

--- Project/Logic/BallLogic.cs
+++ Project/Logic/BallLogic.cs
@@ -22,8 +22,7 @@ namespace Logic
         private Timer _timer;
         private ModelRepo _repoModel = new ModelRepo();
         private DataRepo _repoData = new DataRepo();
-
-
+        /*
         private void GenerateDirection()
         {
             int counter = 0;
@@ -35,6 +34,16 @@ namespace Logic
                 ball.Direction = rand == 0 ? -1 : 1;
                 counter++;
             }
+        }
+        */
+        private void GenerateDirection(object obj1, object obj2)
+        {
+            BallModel ball = (BallModel)obj1;
+            BallData ballData = (BallData)obj2;
+            ballData.A = _random.NextDouble() * 2 - 1;
+            ballData.B = ball.PosY - (ballData.A * ball.PosX);
+            int rand = _random.Next(0, 2);
+            ballData.Direction = rand == 0 ? -1 : 1;
         }
 
         private void MoveBalls(object state)
@@ -68,9 +77,41 @@ namespace Logic
             }
         }
 
+        private void MoveBall(object state1, object state2)
+        {
+            BallModel ball = (BallModel)state1;
+            BallData ballData = (BallData)state2;
+            while (true)
+            {
+                double newX, newY;
+
+                lock (ball) // Blokuj dostęp do obiektu kuli podczas aktualizacji jej pozycji
+                {
+                    newX = ball.PosX + ballData.Direction * 4;
+                    newY = ballData.A * newX + ballData.B;
+
+                    if (newX >= 0 && newX <= _maxWidth && newY >= 0 && newY <= _maxHeight)
+                    {
+                        ball.PosX = newX;
+                        ball.PosY = newY;
+                    }
+                    else
+                    {
+                        if (newX < 0 || newX > _maxWidth)
+                        {
+                            ballData.Direction = -ballData.Direction;
+                        }
+                        ballData.A = -ballData.A;
+                        ballData.B = ball.PosY - (ballData.A * ball.PosX);
+                    }
+                }
+                Thread.Sleep(10);
+            }
+        }
+           
         public void CreateBalls()
         {
-            
+            // Nw czy po kolejnym wywołaniu tej funkcji poprzednie wątki się kończą
             if (_timer != null)
             {
                 _timer.Dispose();
@@ -80,19 +121,20 @@ namespace Logic
             RepoModel.Balls.Clear();
             
             int ballsNumber = Convert.ToInt32(RepoModel.BallsNumber);
-            
+
             for (int i = 0; i < ballsNumber; i++)
             {
                 BallModel ball = new BallModel();
                 BallData ballData = new BallData();
                 ball.PosX = _random.Next(0, (int)(_maxWidth));
-                ball.PosY = _random.Next(0, (int)(_maxHeight)); 
+                ball.PosY = _random.Next(0, (int)(_maxHeight));
                 RepoModel.Balls.Add(ball);
                 RepoData.Balls.Add(ballData);
+                GenerateDirection(ball, ballData);
+                Thread thread = new Thread(() => MoveBall(ball, ballData)); 
+                thread.IsBackground = true;
+                thread.Start();
             }
-
-            GenerateDirection();
-            _timer = new Timer(MoveBalls, null, 0, 10);
         }
         
         
